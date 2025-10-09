c14dd9c5e1c1289b6a301b2728c8333410250886
Lucas Nicosia <lnicosia@popcornfx.com>
2025-10-09 12:15:16 +0200

    Updated effects to v2.22

--- Assets/make_ue_content_examples_shortcut.bat
+++ Assets/make_ue_content_examples_shortcut.bat
@@ -2,9 +2,9 @@
 
 echo ---------------------------------------------------------------------
 echo -
-echo -  Creating UE4 Plugin junction WITH %PK_SDK_ROOT%/source_tree/Integrations/UE4/
+echo -  Creating UE Plugin junction WITH %PK_SDK_ROOT%release\application\Packs\ContentExamples
 echo -
 echo ---------------------------------------------------------------------
 
-mklink /J ExampleEffects "%PK_SDK_ROOT%/release/application/Packs/ContentExamples"
+mklink /J "ExampleEffects" "%PK_SDK_ROOT%release\application\Packs\ContentExamples"
 pause
